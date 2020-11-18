47e4fca57a95cd7187a073a181dbafe76ec1dc82
Vladimir Filipovic <vladofilipovic@hotmail.com>
2020-11-18 12:49:18 +0100

    dodaodijagrame kasa

---
+++ Src/KojackGames.Blackjack/KojackGames.Blackjack.Domain/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KojackGames.Blackjack.Domain
+{
+    public class Class1 : GamePlay.Model.Dealer.Actions.DealerActionOnPlayersHand
+    {
+    }
+}
